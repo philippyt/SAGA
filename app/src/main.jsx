5bd05b84ac03eea42e3e7f35c74841c67e0b9044
philippyt <philipaha@proton.me>
2026-02-19 21:49:01 +0100

    v1: simple frontend

---
+++ app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainFront from "./main_front";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MainFront />
+  </React.StrictMode>
+);
